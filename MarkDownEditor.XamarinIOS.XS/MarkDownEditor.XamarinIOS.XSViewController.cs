3a9c1212d49ce4d9038065e9d48fae4f6d7a9ba2
moljac <mcvjetko@holisticware.net>
2014-04-08 08:50:59 +0200

    Demo scafolding

---
+++ MarkDownEditor.XamarinIOS.XS/MarkDownEditor.XamarinIOS.XSViewController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MarkDownEditor.XamarinIOS.XS
+{
+	public partial class MarkDownEditor_XamarinIOS_XSViewController : UIViewController
+	{
+		static bool UserInterfaceIdiomIsPhone
+		{
+			get
+			{
+				return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone;
+			}
+		}
+
+		public MarkDownEditor_XamarinIOS_XSViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+		}
+
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+		}
+
+		#endregion
+	}
+}
+
